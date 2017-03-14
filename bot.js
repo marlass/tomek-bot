3bdec7eea406601293d1ede39b37a76a5f71a5e5
marlass <lasakmarcin90@gmail.com>
2017-03-14 20:19:22 +0100

    Add templates

--- bot.js
+++ bot.js
@@ -1,6 +1,31 @@
 const botBuilder = require('claudia-bot-builder');
+const fbTemplate = botBuilder.fbTemplate;
 
 module.exports = botBuilder(request => {
+    if (request.type === 'facebook') {
+        if (request.text === 'quick') {
+            const newMessage = new fbTemplate.Text('What\'s your favorite House in Game Of Thrones');
+
+            return newMessage
+            .addQuickReply('Stark', 'STARK')
+            .addQuickReply('Lannister', 'LANNISTER')
+            .addQuickReply('Targaryen', 'TARGARYEN')
+            .addQuickReply('None of the above', 'OTHER')
+            .get();
+        } else if (request.text === 'generic') {
+            const generic = new fbTemplate.Generic();
+
+            return generic
+            .addBubble('Claudia.js', 'Deploy Node.js microservices to AWS easily')
+                .addUrl('https://claudiajs.com')
+                .addButton('Say hello', 'HELLO')
+                .addButton('Go to Github', 'https://github.com/claudiajs/claudia')
+            .addBubble('Claudia Bot Builder')
+                .addButton('Go to Github', 'https://github.com/claudiajs/claudia-bot-builder')
+            .get();
+        }
+    }   
+
     if (request.text.includes("?")) {
         return 'Nie wiem'
     } else if (request.text.includes("kurwa") ||
